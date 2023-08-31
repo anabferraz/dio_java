e2e63a872e6c59b635000735258a7e6b1b4eee4d
anabferraz <ferraz.anab@gmail.com>
2023-08-30 23:00:09 -0300

    criada a classe Candidato

---
+++ Controle_candidatos/ProcessoSeletivo.java
@@ -0,0 +1,19 @@
+package Controle_candidatos;
+import java.util.Scanner;
+
+public class ProcessoSeletivo {
+
+    public static void main (String [] args){
+        Candidato x = new Candidato();
+        System.out.println("Processo seletivo XPTO: ");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o seu nome completo: ");
+        x.nome = scan.nextLine();
+        System.out.println("Digite sua pretensão salarial: ");
+        x.salarioPret = scan.nextDouble();
+
+        analisar_Candidato();
+    }
+    
+    
+}
