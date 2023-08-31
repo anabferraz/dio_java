db74aa05d0b58a6c0d531ee7f2df8c7715052635
anabferraz <ferraz.anab@gmail.com>
2023-08-30 21:56:29 -0300

    bug fixed\newline char left in buffer consumed.

--- Projeto_java_basico_dio/ContaTerminal.java
+++ Projeto_java_basico_dio/ContaTerminal.java
@@ -8,12 +8,29 @@ public class ContaTerminal{
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite o número da agência e, em seguida, aperte ENTER: ");
         x.agencia = scan.nextInt();
-        System.out.println("Digite o número da conta com traço e dígito e, em seguida, aperte ENTER: ");
-        x.conta = scan.nextLine();
-        System.out.println("Digite o seu nome completo, em seguida, aperte ENTER: ");
-        x.nome = scan.nextLine();
-        System.out.println("Digite o seu saldo atual, em seguida, aperte ENTER: ");
-        x.saldo = scan.nextFloat();
+        scan.nextLine();
+        if (x.agencia != 0){
+            System.out.println("Digite o número da conta com traço e dígito e, em seguida, aperte ENTER: ");
+            x.conta = scan.nextLine();
+            if (x.conta != ""){
+                System.out.println("Digite o seu nome completo, em seguida, aperte ENTER: ");
+                x.nome = scan.nextLine();
+                if (x.nome != ""){
+                    System.out.println("Digite o seu saldo atual, em seguida, aperte ENTER: ");
+                    x.saldo = scan.nextFloat();
+                }
+                else {
+                    System.out.println("Você precisa digitar um valor válido. Tente novemente!");
+                }
+            
+            }
+            else {
+                System.out.println("Você precisa digitar um valor válido. Tente novemente!");
+                }
+        }
+
+
+
         x.print_values();
     }
 }
